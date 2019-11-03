03a03955d168229fc37b64abdbb5d256a92a3f28
SharlyYannis <yannis.aubry@4g-technology.eu>
2019-11-03 16:50:40 +0100

    Add parse_round_details_to_db capability

--- superffe.sh
+++ superffe.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# TODO Struct db of seasons, compets, divisions and groups
-# TODO Struct db of teams and games (v2 clubs and players)
-
-# TODO Build db SCDG
+# TODO Build db of seasons, compets, divisions and groups
 
 # Loop over SCDG
 
@@ -12,9 +9,14 @@ division=7
 group=879
 season=2019
 
-[ ! -d "./results" ] && mkdir ./results
+[ ! -d "results" ] && mkdir results
+[ -e "results/superffe.sqlite" ] && rm results/superffe.sqlite
+
 casperjs dump_teams.js --compet=$compet --division=$division --group=$group --season=$season | grep "^[1-9]\|group" > results/teams.tmp
-casperjs dump_round_details.js --compet=$compet --division=$division --group=$group --season=$season | grep -v "^Comp\|^TypeError" > results/round_details.tmp
+
+casperjs dump_round_details.js --compet=$compet --division=$division --group=$group --season=$season | grep -v "^Comp\|^TypeError" | sed 's/\xC2\xA0/ /g' > results/round_details.tmp
 
 python3 parse_teams_to_db.py --file=results/teams.tmp --db=results/superffe.sqlite
+sqlite3 results/superffe.sqlite "SELECT nom FROM equipes"
+
 python3 parse_round_details_to_db.py --file=results/round_details.tmp --db=results/superffe.sqlite
