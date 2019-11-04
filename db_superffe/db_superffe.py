13a98f5b578d31b856214b526f4f62feca5622db
SharlyYannis <yannis.aubry@4g-technology.eu>
2019-11-04 20:24:49 +0100

    Refactoring

--- db_superffe/db_superffe.py
+++ db_superffe/db_superffe.py
@@ -1,82 +1,84 @@
 import sqlite3
 
-def init_db(db_file):
+def _init_db(db_file):
     db = sqlite3.connect(db_file)
     cursor = db.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS TEAMS (labelTeam text, placeTeam int, pointsTeam int, j int, d int, p int, c int, uNumGroup int)")
-    cursor.execute("CREATE TABLE IF NOT EXISTS MATCHS (uNumMatch int, team1 text, team2 text, scoreMatch text, uNumRound int)")
-    cursor.execute("CREATE TABLE IF NOT EXISTS GAMES (player1 text, elo1 int, player2 text, elo2 int, numBoard int, scoreGame text, uNumMatch int)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS TEAMS (labelTeam text, placeTeam int, pointsTeam int, j int, d int, p int, c int, uidGroup text)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS ROUNDS (uidRound text, int numRound, uidGroup text)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS MATCHS (uidMatch text, team1 text, team2 text, scoreMatch text, uidRound text)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS GAMES (player1 text, elo1 int, player2 text, elo2 int, numBoard int, scoreGame text, uidMatch text)")
     return db, cursor
 
-def parse_teams(input_file, db_file):
-    db, cursor = init_db(db_file)
-    (group, division, compet, season) = ("", "", "", "")
-    f = open(input_file, 'r')
-    for line in f:
-        if(line[-1:] is '\n'): line = line[:-1]
-        if('group' in line):
-            group = line.split('group:')[1].split(',')[0]
-            division = line.split('division:')[1].split(',')[0]
-            compet = line.split('compet:')[1].split(',')[0]
-            season = line.split('season:')[1]
-        else:
-            (place, label, points, j, d, p, c) = line.split('\t')
-            values= "('" + label + \
-                    "'," + place + \
-                    "," + points + \
-                    "," + j + \
-                    "," + d + \
-                    "," + p + \
-                    "," + c + \
-                    "," + group + ")"
-            cursor.execute("INSERT INTO TEAMS VALUES " + values)
-            
+def _close_and_commit(db):
     db.commit()
     db.close()
-    return
 
-def parse_round_details(input_file, db_file):
-    db, cursor = init_db(db_file)
+def _concat_game_values(actor1, actor2, board, score, uidMatch):
+    (player1, elo1) = actor1.rsplit(' ', 1)
+    (player2, elo2) = actor2.rsplit(' ', 1)
+    return "('" + player1 + "'," + elo1 + ",'" + player2 + "'," + elo2 + \
+            "," + str(board) + ",'" + score + "','" + str(uidMatch) + "')"
+
+def _concat_match_values(num_match, actor1, actor2, score, num_round):
+    return "('" + str(num_match) + "','" + actor1 + "','" + actor2 + \
+            "','" + score + "','" + num_round + "')"
+
+def _concat_team_values(line, group):
+    (place, label, points, j, d, p, c) = line.split('\t')
+    return "('" + label + "'," + place + "," + points + "," + j + \
+            "," + d + "," + p + "," + c + ",'" + group + "')"
+
+def _split_descriptor(line):
+    group = line.split('group:')[1].split(',')[0]
+    division = line.split('division:')[1].split(',')[0]
+    compet = line.split('compet:')[1].split(',')[0]
+    season = line.split('season:')[1]
+    return (group, division, compet, season)
+   
+def get_team_labels(cursor):
     teams = []
     for team_array in cursor.execute("SELECT labelTeam FROM TEAMS"):
         teams.append(team_array[0])
+    return teams
 
-    f = open(input_file, 'r')
-    num_match = 0
-    (num_round, group, division, compet, season) = ("", "", "", "", "")
-    for line in f:
+def create_db(db_file):
+    db, cursor = _init_db(db_file)
+    _close_and_commit(db)
+
+def parse_teams(input_file, db_file):
+    db, cursor = _init_db(db_file)
+    (group, division, compet, season) = ("", "", "", "")
+    for line in open(input_file, 'r'):
         if(line[-1:] is '\n'): line = line[:-1]
-        if(line[-1:] is '\t'): line = line[:-1]
-        if(line is ''): continue
         if('group' in line):
-            num_round = line.split('round:')[1].split(',')[0]
-            group = line.split('group:')[1].split(',')[0]
-            division = line.split('division:')[1].split(',')[0]
-            compet = line.split('compet:')[1].split(',')[0]
-            season = line.split('season:')[1]
+            (group, division, compet, season) = _split_descriptor(line)
         else:
-            try:
+            cursor.execute("INSERT INTO TEAMS VALUES " + _concat_team_values(line, group))
+    _close_and_commit(db)            
+
+def parse_round_details(input_file, db_file):
+    db, cursor = _init_db(db_file)
+    teams = get_team_labels(cursor)
+    num_round = ""
+    num_match = 0
+    for line in open(input_file, 'r'):
+        try:
+            if(line[-1:] is '\n'): line = line[:-1]
+            if(line[-1:] is '\t'): line = line[:-1]
+            if(line is ''): continue
+            print(line)
+            if('group' in line):
+                num_round = line.split('round:')[1].split(',')[0]
+                num_match = 0
+            else:
                 (actor1, score, actor2) = line.split('\t')
                 if(actor1 in teams and actor2 in teams):
                     num_match += 1
-                    values= "(" + str(num_match) + \
-                            ",'" + actor1 + \
-                            "','" + actor2 + \
-                            "','" + score + \
-                            "," + num_round + ")"
+                    values = _concat_match_values(num_match, actor1, actor2, score, num_round)
+                    print(values)
                     cursor.execute("INSERT INTO MATCHS VALUES " + values)
                 else:
-                    (player1, elo1) = actor1.rsplit(' ', 1)
-                    (player2, elo2) = actor2.rsplit(' ', 1)
-                    values= "('" + player1 + \
-                            "'," + elo1 + \
-                            ",'" + player2 + \
-                            "'," + elo2 + \
-                            "," + str(9999) + \
-                            ",'" + score + \
-                            "'," + str(num_match) + ")"
-                    cursor.execute("INSERT INTO GAMES VALUES " + values)
-            except: pass
-
-    db.commit()
-    db.close()
+                    cursor.execute("INSERT INTO GAMES VALUES " + _concat_game_values(actor1, actor2, 9999, score, num_match))
+        except:
+            pass
+    _close_and_commit(db)
--- superffe.sh
+++ superffe.sh
@@ -12,11 +12,10 @@ season=2019
 [ ! -d "results" ] && mkdir results
 [ -e "results/superffe.sqlite" ] && rm results/superffe.sqlite
 
-#casperjs dump_teams.js --compet=$compet --division=$division --group=$group --season=$season | grep "^[1-9]\|group" > results/teams.tmp
+casperjs dump_teams.js --compet=$compet --division=$division --group=$group --season=$season | grep "^[1-9]\|group" > results/teams.tmp
 
-#casperjs dump_round_details.js --compet=$compet --division=$division --group=$group --season=$season | grep -v "^Comp\|^TypeError" | sed 's/\xC2\xA0/ /g' > results/round_details.tmp
+casperjs dump_round_details.js --compet=$compet --division=$division --group=$group --season=$season | grep -v "^Comp\|^TypeError" | sed "s/\xC2\xA0\|'/ /g" > results/round_details.tmp
 
-python3 -c'from db_superffe import parse_teams; parse_teams("results/teams.tmp", "results/superffe.sqlite")'
+python3 -c'import db_superffe; db_superffe.parse_teams("results/teams.tmp", "results/superffe.sqlite"); db_superffe.parse_round_details("results/round_details.tmp", "results/superffe.sqlite")'
 sqlite3 results/superffe.sqlite "SELECT labelTeam FROM TEAMS"
 
-python3 -c'from db_superffe import parse_round_details; parse_round_details("results/round_details.tmp", "results/superffe.sqlite")'
